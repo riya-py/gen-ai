dc44b647e600e4cd2a606e04c50e8006a031bdb2
riya-py <lowkeywhocares@gmail.com>
2026-07-29 19:17:48 +0530

    made chatbot

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -7,11 +7,19 @@ model = ChatMistralAI(
     model="mistral-large-latest",
     temperature=0.7
 )
+
+messages=[
+
+]
+
 print("--------------------------Welcome type 0 to exit the application----------------------------")
 while True:
     prompt=input("you: ")
+    messages.append(prompt)
     if(prompt=="0"):
         break
     response=model.invoke(prompt)
-
+    messages.append(response.content)
     print("Bot: ",response.content)
+
+print(messages)   # prints the message
