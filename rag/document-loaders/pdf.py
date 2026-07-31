dd83cbf0c8c26a935dc4e932e056ad90870a48d2
riya-py <lowkeywhocares@gmail.com>
2026-07-31 15:05:56 +0530

    updated pdf and test

--- rag/document-loaders/pdf.py
+++ rag/document-loaders/pdf.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import TokenTextSplitter
 
 load_dotenv()
 
@@ -7,4 +8,7 @@ data = PyPDFLoader("GRU.pdf")
 
 docs=data.load()
 
-print(docs[14])
+splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=10)
+chunks = splitter.split_documents(docs)
+
+print(chunks[0].page_content)
--- rag/document-loaders/test.py
+++ rag/document-loaders/test.py
@@ -1,7 +1,16 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+
+splitter=CharacterTextSplitter(
+    separator="",
+    chunk_size=10, 
+    chunk_overlap=1)
 
 loader = TextLoader("notes.txt")
 docs = loader.load()
+splits = splitter.split_documents(docs)
 
-
-print(docs)
+for i in splits:
+    print(i.page_content)
+    print("----")
+    print("")
