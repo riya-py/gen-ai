0a518ae1ee75ba5cddeda33b006ac572e1c5efca
riya-py <lowkeywhocares@gmail.com>
2026-07-31 15:09:52 +0530

    updated pdf python file

--- rag/document-loaders/pdf.py
+++ rag/document-loaders/pdf.py
@@ -9,6 +9,6 @@ data = PyPDFLoader("GRU.pdf")
 docs=data.load()
 
 splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=10)
-chunks = splitter.split_documents(docs)
+splits = splitter.split_documents(docs)
 
-print(chunks[0].page_content)
+print(splits[0].page_content)
